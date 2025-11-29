a987b85a743b1a9003e177a54dc2ca569aec75b0
Steopa2001 <moraru495@gmail.com>
2025-11-29 12:02:07 +0100

    [feat] Add Dipendente type alias with validated fields and contract constraints

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 // 🏆 Snack 1
-
 // Hai ricevuto un dato generico da un'API, ma non sai di che tipo sia… Il tuo compito è controllare il tipo del dato e stampare il valore in modo corretto.
 //     Se è una stringa: stampala in maiuscolo
 //     Se è un numero: moltiplicalo per due e stampalo
@@ -26,3 +25,27 @@ if (typeof dato === "string") {
 } else {
   console.log("Tipo non supportato");
 }
+
+// --------------------------------------------------------------------------------------------------------------
+
+// 🏆 Snack 2
+// Crea un type alias Dipendente che rappresenta un lavoratore con i seguenti dati:
+//     nome → stringa
+//     cognome → stringa
+//     annoNascita → numero
+//     sesso → Può essere solo "m" o "f".
+//     anniDiServizio (array di numeri, es. [2014, 2015, 2017, 2018])
+// 🎯 BONUS
+// Il type alias Dipendente, ha anche i seguenti dati:
+//     emailAziendale → Email assegnata al dipendente (non si può modificare)
+//     contratto → Specifica il tipo di contratto del dipendente, con valori limitati a “indeterminato”, “determinato” o “freelance”.
+
+type Dipendente = {
+  nome: string;
+  cognome: string;
+  annoNascita: number;
+  sesso: "m" | "f";
+  anniDiServizio: number[];
+  readonly emailAziendale: string;
+  contratto: "indeterminato" | "determinato" | "freelance";
+};
