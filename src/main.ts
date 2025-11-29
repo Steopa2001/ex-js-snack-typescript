0046c6becfdb94af3991c579f31d063ea8d04b2e
Steopa2001 <moraru495@gmail.com>
2025-11-29 11:54:57 +0100

    [feat] Add dynamic data handling with type-based output formatting

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,28 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// 🏆 Snack 1
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// Hai ricevuto un dato generico da un'API, ma non sai di che tipo sia… Il tuo compito è controllare il tipo del dato e stampare il valore in modo corretto.
+//     Se è una stringa: stampala in maiuscolo
+//     Se è un numero: moltiplicalo per due e stampalo
+//     Se è un booleano: stampa “Sì” o “No” in base al suo valore
+//     In tutti gli altri casi: stampa “Tipo non supportato”
+// 🎯 BONUS
+//     Se è null: stampa “Il dato è vuoto”
+//     Se è un array: stampa la sua lunghezza
+//     Se è una Promise: attendi che si risolva e stampa il valore del resolve.
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let dato: unknown;
+if (typeof dato === "string") {
+  console.log(dato.toUpperCase());
+} else if (typeof dato === "number") {
+  console.log(dato * 2);
+} else if (typeof dato === "boolean") {
+  console.log(dato ? "Si" : "No");
+} else if (dato === null) {
+  console.log("Il dato è vuoto");
+} else if (dato instanceof Array) {
+  console.log(dato.length);
+} else if (dato instanceof Promise) {
+  dato.then((msg) => console.log(msg));
+} else {
+  console.log("Tipo non supportato");
+}
