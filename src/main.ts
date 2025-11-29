a560f6bb7cc2a065c3dbb986e612f565a47f339e
Steopa2001 <moraru495@gmail.com>
2025-11-29 12:10:35 +0100

    [feat] Extend Dipendente with Developer and ProjectManager specialized roles

--- src/main.ts
+++ src/main.ts
@@ -40,6 +40,29 @@ if (typeof dato === "string") {
 //     emailAziendale → Email assegnata al dipendente (non si può modificare)
 //     contratto → Specifica il tipo di contratto del dipendente, con valori limitati a “indeterminato”, “determinato” o “freelance”.
 
+// type Dipendente = {
+//   nome: string;
+//   cognome: string;
+//   annoNascita: number;
+//   sesso: "m" | "f";
+//   anniDiServizio: number[];
+//   readonly emailAziendale: string;
+//   contratto: "indeterminato" | "determinato" | "freelance";
+// };
+
+// --------------------------------------------------------------------------------------------------------------
+
+// 🏆 Snack 3
+// Estendiamo Dipendente per definire due ruoli specifici all'interno dell'azienda:
+//     Developer
+//         livelloEsperienza → Il livello di esperienza del developer (le scelte possibili sono solo “Junior”, “Mid” o “Senior”).
+//         linguaggi → Un array contenente i linguaggi di programmazione utilizzati dal developer in azienda (opzionale, perché i neo assunti non hanno ancora dei linguaggi assegnati).
+//         certificazioni → Un array di stringhe contenente certificazioni tecniche ottenute dal developer (può essere vuoto).
+//     ProjectManager
+//         teamSize → Il numero di persone nel team gestito dal Project Manager (può essere null se non ha ancora un team assegnato).
+//         budgetGestito → Il totale del budget annuale gestito dal PM (opzionale).
+//         stakeholderPrincipali → Un array di stringhe con i nomi dei principali stakeholder con cui il PM collabora (può essere vuoto).
+
 type Dipendente = {
   nome: string;
   cognome: string;
@@ -48,4 +71,16 @@ type Dipendente = {
   anniDiServizio: number[];
   readonly emailAziendale: string;
   contratto: "indeterminato" | "determinato" | "freelance";
+};
+
+type Developer = Dipendente & {
+  livelloEsperienza: "Junior" | "Mid" | "Senior";
+  linguaggi?: string[];
+  certificazioni: string[];
+};
+
+type ProjectManager = Dipendente & {
+  teamSize: number | null;
+  budgetGestito?: number;
+  stakeholderPrincipali: string[];
 };
